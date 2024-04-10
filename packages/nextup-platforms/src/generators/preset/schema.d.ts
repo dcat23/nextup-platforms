fa69c904ca1b53c6f815388565c70a3a9d0c4a4e
Devin Catuns <devincatuns1@gmail.com>
2024-04-10 17:28:29 -0400

    v0.0.2: add generator schema

--- packages/nextup-platforms/src/generators/preset/schema.d.ts
+++ packages/nextup-platforms/src/generators/preset/schema.d.ts
@@ -1,3 +1,11 @@
 export interface PresetGeneratorSchema {
   name: string;
+  scope: string;
+  website: string;
+  fileName: string;
+  className: string;
+  propertyName: string;
+  constantName: string;
+  snakeCaseName: string;
+  title: string;
 }
